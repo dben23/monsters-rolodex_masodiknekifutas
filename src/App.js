5b06d875ccaf0af519957d442f79884195345964
dben23 <drozdik.beni@gmail.com>
2022-05-07 17:13:00 +0200

    update after lesson #038

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+// import CardList from "./components/card-list/card-list.component";
 
-function App() {
-  return <h1>sziaUram</h1>;
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./card.styles.css";
+
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+};
+
+//******    íGY IS LEHET  ******** */
+
+// function Card(props) {
+//   return (
+//     <div className="card-container">
+//       <img
+//         alt="monster"
+//         src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+//       />
+//       <h2>{props.monster.name}</h2>
+//       <p>{props.monster.email}</p>
+//     </div>
+//   );
+// }
+
+// export default Card;
